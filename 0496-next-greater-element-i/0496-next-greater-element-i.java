e34dc0ce9cbf94311e4133e553272df61cb253b0
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-01-19 10:14:42 +0600

    Time: 3 ms (89.60%), Space: 42.5 MB (69.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        Stack<Integer> stk = new Stack<>();
+        
         int[] ret = new int[nums1.length];
         
-        for (int i = 0; i < nums1.length; i++) {
-            
-            ret[i] = -1;
-            boolean found = false;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    //System.out.println(nums1[i] + " == " + nums2[j]);
-                    found = true;
-                }
-                
-                if (found && nums1[i] < nums2[j]) {
-                    //System.out.println(nums1[i] + " < " + nums2[j]);
-                    ret[i] = nums2[j];
-                    break;
-                }
+        for (int t: nums2) {
+            while (!stk.isEmpty() && stk.peek() < t) {
+                mp.put(stk.pop(), t);
             }
-            
+            stk.push(t);
         }
         
+        for (int i = 0; i < nums1.length; i++) {
+            ret[i] = mp.getOrDefault(nums1[i], -1);
+        }
+        
+        
         return ret;
+        
     }
 }
