39ce9670208f36200f7c18b68c416ab05ca28081
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-01-28 12:21:17 +0600

    Time: 8 ms (27.35%), Space: 44.5 MB (14.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -21,3 +21,34 @@ class Solution {
         
     }
 }
+
+/*
+Time: 6 ms
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ret = new int[nums1.length];
+        
+        for (int i = 0; i < nums1.length; i++) {
+            
+            ret[i] = -1;
+            boolean found = false;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    //System.out.println(nums1[i] + " == " + nums2[j]);
+                    found = true;
+                }
+                
+                if (found && nums1[i] < nums2[j]) {
+                    //System.out.println(nums1[i] + " < " + nums2[j]);
+                    ret[i] = nums2[j];
+                    break;
+                }
+            }
+            
+        }
+        
+        return ret;
+    }
+}
+
+*/
