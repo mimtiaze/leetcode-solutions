0eaf67b657e039fe0963f725bb62f6bda5d9fa19
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-02-05 17:04:53 +0600

    Time: 1 ms (99.91%), Space: 45.9 MB (70.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -15,6 +15,38 @@ class Solution {
 
 /*
 Method: Boyer-Moore Voting Algorithm
+Time: 1ms
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+        
+        for (int num : nums) {
+            if (count == 0) 
+                candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        
+        return candidate;
+    }
+}
 
 
+Method: Map
+Time: 34ms
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mp;
+        for(auto c: nums)
+            mp[c]++;
+        
+        int ret = nums[0];
+        int threshold = nums.size()/2;
+        for(auto c: mp)
+            if(c.second>threshold)
+                ret = c.first;
+        return ret;
+    }
+};
 */
