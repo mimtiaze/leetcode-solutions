cc1716efc7df77c09f158f47947afd45ffbd306e
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-04-11 20:02:58 +0600

    Time: 39 ms (89.07%), Space: 47 MB (29.38%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -18,6 +18,26 @@ class Solution {
 }
 
 /*
+Method: Brute Force (Java)
+Time: 38ms
+public String removeStars(String s) {
+    LinkedList<Character> l = new LinkedList<>();
+    for (char ch: s.toCharArray()) {
+        if (ch == '*') {
+            l.removeLast();
+        } else {
+            l.addLast(ch);
+        }
+    }
+
+    StringBuilder strbuilder = new StringBuilder();
+    for (char ch : l) {
+        strbuilder.append(ch);
+    }
+    return strbuilder.toString();
+}
+
+
 Method: Brute Force
 TIme: 79ms
 
