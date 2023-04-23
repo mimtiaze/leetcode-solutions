d7bc9efee155af22eabdc74808966dbecbdfeb6f
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-04-23 07:38:59 +0600

    Time: 0 ms (100.00%), Space: 40.9 MB (82.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -23,26 +23,6 @@ class Solution {
 
 
 /*
-Method: HashMap
-Time: 1ms
-
-class Solution {
-    public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        
-        for (int t : nums) {
-            mp.merge(t, 1, Integer::sum);
-        }
-        
-        int index = 0;
-        for (Map.Entry<Integer, Integer> tmp : mp.entrySet()) {
-            for (int i = 0; i < tmp.getValue(); i++) {
-                nums[index++] = tmp.getKey();
-            }
-        }
-    }
-}
-
 
 Method: Array
 Time: 0ms
@@ -69,5 +49,29 @@ class Solution {
         }
     }
 }
+
+
+Method: HashMap
+Time: 1ms
+
+class Solution {
+    public void sortColors(int[] nums) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for (int t : nums) {
+            mp.merge(t, 1, Integer::sum);
+        }
+        
+        int index = 0;
+        for (Map.Entry<Integer, Integer> tmp : mp.entrySet()) {
+            for (int i = 0; i < tmp.getValue(); i++) {
+                nums[index++] = tmp.getKey();
+            }
+        }
+    }
+}
+
+
+
 
 */
