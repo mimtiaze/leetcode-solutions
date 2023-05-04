3eba74912494a7061432e2b491ac3ded54bd912f
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-05-04 08:45:33 +0600

    Time: 16 ms (27.76%), Space: 43.6 MB (29.35%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,22 +1,41 @@
 class Solution {
-    List<Integer> getElementsOnlyInFirstList(int[] nums1, int[] nums2) {
-        Set<Integer> onlyInNums1 = new HashSet<> (); 
-        Set<Integer> existsInNums2 = new HashSet<> (); 
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> mp1 = new HashMap<>();
+        HashMap<Integer, Integer> mp2 = new HashMap<>();
         
-        for (int num : nums2) {
-            existsInNums2.add(num);
+        for (int t : nums1) {
+            mp1.put(t, t);
         }
         
-        for (int num : nums1) {
-            if (!existsInNums2.contains(num)) {
-                onlyInNums1.add(num);
+        for (int t : nums2) {
+            mp2.put(t, t);
+        }
+        
+        //mp1.forEach((k, v) -> System.out.println(k));
+        
+        List<List<Integer>> ret = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        for (int t: nums1) {
+            if (!mp2.containsKey(t)) {
+                set.add(t);
             }
         }
+        //list.forEach((k) -> System.out.print(k + " ")); System.out.println();
+        for (int t: set) list.add(t);
+        ret.add(list);
         
-        return new ArrayList<>(onlyInNums1);
-    }
-    
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        return Arrays.asList(getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1));
+        list = new ArrayList<>();
+        set = new HashSet<>();
+        for (int t: nums2) {
+            if (!mp1.containsKey(t)) {
+                set.add(t);
+            }
+        }
+        //list.forEach((k) -> System.out.print(k + " ")); System.out.println();
+        for (int t: set) list.add(t);
+        ret.add(list);
+        
+        return ret;
     }
 }
