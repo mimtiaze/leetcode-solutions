cdcd282d1f3f5df975dff5768d5071df42dfba4f
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-10-29 23:43:08 +0600

    Time: 4 ms (53.09%), Space: 6.1 MB (89.71%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,8 @@
+// Mathmetical
+// Took help from discussion tab
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
